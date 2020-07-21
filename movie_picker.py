f79aadb0b026ee3e3bd536515800bb07ad2f1ed8
Christopher Escobedo <chrisescobedo0617@gmail.com>
2020-07-21 14:12:44 -0400

    fixed my api being leaked and also added a function that would fetch the genres instead of having it hard coded. I also added a way for the user to select what actor to search for in the movies being returned.

--- movie_picker.py
+++ movie_picker.py
@@ -1,7 +1,10 @@
-import requests
-from genres import movie_genres
+import os
 import sys
 import argparse
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
 def get_movies(url):
@@ -22,19 +25,34 @@ def get_overview(url):
         print(value.get('overview'))
         print('\n')
 
+
 def get_nowplaying():
     """Function to get a list movies currently playing"""
-    np_url = 'https://api.themoviedb.org/3/movie/now_playing?api_key=54bb31247dabf5791bce265c2fa2cd4f&language=en-US&page=1'
+    np_url = f'https://api.themoviedb.org/3/movie/now_playing?api_key={os.environ["API_KEY"]}&language=en-US&page=1'
     response = requests.get(np_url)
     json_rsponse = response.json()
-    print(np_url)
     for value in json_rsponse.get('results'):
         print(value.get('title'))
 
 
-def genre_to_int(genre):
-    inetger = movie_genres.get(genre)
-    return inetger
+def get_genre_int(user_genre):
+    genres_url = f'https://api.themoviedb.org/3/genre/movie/list?api_key={os.environ["API_KEY"]}&language=en-US'
+    response = requests.get(genres_url)
+    genres = response.json().get('genres')
+    for genre in genres:
+        if genre.get('name').lower() == user_genre:
+            return genre.get('id')
+    return None
+
+
+def get_actor_int(user_actor):
+    actor_url = f'https://api.themoviedb.org/3/search/person?api_key={os.environ["API_KEY"]}&language=en-US&query={user_actor}&page=1&include_adult=false'
+    response = requests.get(actor_url)
+    actors = response.json().get('results')
+    for actor in actors:
+        if actor.get('name').lower() == user_actor:
+            return actor.get('id')
+    return None
 
 
 def create_parser():
@@ -46,10 +64,13 @@ def create_parser():
     parser.add_argument(
         '-y', '--year', help='Release year of movies the user wants to watch')
     parser.add_argument(
-        '-np', '--nowplaying', action='store_true', help='Movies that are currently playing'
+        '-n', '--nowplaying', action='store_true', help='Movies that are currently playing'
     )
     parser.add_argument(
-        '-ov', '--overview', action='store_true', help='Provides and overview of what the movies are about'
+        '-o', '--overview', action='store_true', help='Provides and overview of what the movies are about'
+    )
+    parser.add_argument(
+        '-a', '--actor', help='Get movies featuring an actor'
     )
     return parser
 
@@ -65,26 +86,38 @@ def main(args):
     
     genre = ns.genre
     year = ns.year
-    url = f'https://api.themoviedb.org/3/discover/movie?api_key=54bb31247dabf5791bce265c2fa2cd4f&language=en-US&sort_by=popularity.desc&page=1'
+    url = f'https://api.themoviedb.org/3/discover/movie?api_key={os.environ["API_KEY"]}&language=en-US&sort_by=popularity.desc&page=1'
 
 
     if ns.genre:
-        genre.lower()
-        g_int = genre_to_int(genre)
-        genre_strip = f'&with_genres={g_int}'
-        url += genre_strip
+        genre_int = get_genre_int(genre.lower())
+        if genre_int:
+            genre_strip = f'&with_genres={genre_int}'
+            url += genre_strip
+        else:
+            print(f'No such genre: {genre}')
+            sys.exit(1)
     if ns.year:
         year_strip = f'&primary_release_year={year}'
         url += year_strip
+    if ns.actor:
+        actor_int = get_actor_int(ns.actor.lower())
+        if actor_int:
+            actor_strip = f'&with_people={actor_int}'
+            url += actor_strip
+        else:
+            print(f'No such actor named: {ns.actor}')
+            sys.exit(1)  
 
-    if ns.genre and not ns.overview or ns.year and not ns.overview:
+    if (ns.genre and not ns.overview) or (ns.year and not ns.overview) or (ns.actor and not ns.overview):
         get_movies(url)
-    if ns.genre and ns.overview or ns.year and ns.overview or ns.year and ns.overview and ns.genre:
+    if (ns.genre and ns.overview) or (ns.year and ns.overview) or (ns.actor and ns.overview) or (ns.year and ns.overview and ns.genre and ns.actor):
         get_overview(url)
 
 
     if ns.nowplaying:
         get_nowplaying()
+
 
 
 if __name__ == '__main__':
